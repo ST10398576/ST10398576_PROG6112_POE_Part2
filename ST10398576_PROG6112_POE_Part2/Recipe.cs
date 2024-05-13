045a25ef74091ed1d33c1623fe7e01b5b26c174f
ST10398576 <130571704+ST10398576@users.noreply.github.com>
2024-05-13 20:49:01 +0000

    Update Recipe.cs

--- ST10398576_PROG6112_POE_Part2/Recipe.cs
+++ ST10398576_PROG6112_POE_Part2/Recipe.cs
@@ -1,6 +1,6 @@
-﻿/*
+/*
  * This is my GitHub repository in case you can't access it through the word doc
- * https://github.com/ST10398576/ST10398576_PROG6112_POE
+ * https://github.com/ST10398576/ST10398576_PROG6112_POE_Part2
  */
 
 using System;
@@ -11,38 +11,43 @@ using System.Threading.Tasks;
 
 namespace ST10398576_PROG6112_POE_Part2
 {
-
+    // Class to represent an ingredient
     public class Ingredient
     {
+        // Properties to store ingredient details
         public string IngredientName { get; set; }
         public double IngredientAmount { get; set; }
         public string IngredientMeasurement { get; set; }
         public double IngredientCalories { get; set; }
         public string IngredientFoodGroup { get; set; }
-
     }
 
     // Class to represent a recipe
     public class Recipe
     {
-        private List<Ingredient> ingredients;
-        private List<string> steps;
-        private double ogScaleAmount;
-        private double currentScaleAmount;
+        // Lists to store ingredients and steps of the recipe
+        public List<Ingredient> ingredients;
+        public List<string> steps;
 
+        // Properties to store original and current scale amounts
+        public double ogScaleAmount;
+        public double currentScaleAmount;
+
+        // Properties to store recipe name and total calories
         public string RecipeName { get; set; }
         public double TotalCalories { get; private set; }
 
-        //Constructor to initialize arrays for ingredients and steps
+        // Constructor to initialize arrays for ingredients and steps
         public Recipe()
         {
             ingredients = new List<Ingredient>();
             steps = new List<string>();
         }
 
-        //Method to add a ingredient to the recipe
+        // Method to add an ingredient to the recipe
         public void addIngredient(string ingredientName, double ingredientAmount, string ingredientMeasurement, double ingredientCalories, string ingredientFoodGroup)
         {
+            // Create a new Ingredient object and add it to the list
             ingredients.Add(new Ingredient
             {
                 IngredientName = ingredientName,
@@ -53,58 +58,82 @@ namespace ST10398576_PROG6112_POE_Part2
             });
         }
 
-        //Method to add a step to the recipe
+        // Method to add a step to the recipe
         public void addStep(string step)
         {
             steps.Add(step);
         }
 
+        // Method to calculate the total calories of the recipe
         public void calculateTotalCalories()
         {
+            // Sum up the calories of all ingredients considering their amounts
             TotalCalories = ingredients.Sum(ingredient => ingredient.IngredientCalories * ingredient.IngredientAmount);
         }
 
-
-        //Method to write the details of the recipe
+        // Method to display the details of the recipe
         public void recipeDisplay()
         {
-            Console.Write($" Recipe: {RecipeName} ");
+            Console.WriteLine($"\n Recipe: {RecipeName} ");
 
-            Console.Write(" Ingredients: ");
+            // Display ingredients
+            Console.WriteLine(" Ingredients: ");
             foreach (var ingredient in ingredients)
             {
-                Console.WriteLine($"-{ingredient.IngredientAmount} {ingredient.IngredientMeasurement} of {ingredient.IngredientName} ({ingredient.IngredientCalories} calories - {ingredient.IngredientFoodGroup})");
+                Console.WriteLine($" - {ingredient.IngredientAmount} {ingredient.IngredientMeasurement} of {ingredient.IngredientName} ({ingredient.IngredientCalories} calories - {ingredient.IngredientFoodGroup})");
             }
-            Console.WriteLine("Steps: ");
+
+            // Display steps
+            Console.WriteLine(" Steps: ");
             foreach (var step in steps)
             {
-                Console.WriteLine($"- {step}");
+                Console.WriteLine($" - {step}");
             }
-            Console.WriteLine($"Total Calories: {TotalCalories}");
+
+            // Display total calories and a warning if they exceed 300
+            Console.WriteLine($" Total Calories: {TotalCalories}");
+            if (TotalCalories > 300)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n Warning! Total calories of the recipe exceed 300!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
         }
 
-        //Method to change the scale of the recipe
+        // Method to change the scale of the recipe
         public void recipeScale(double scaleAmount)
         {
-            // Change scale of a recipe
+            // Update the current scale amount
             currentScaleAmount *= scaleAmount;
-            calculateTotalCalories();
 
+            // Adjust ingredient amounts based on the scaling factor
+            foreach (var ingredient in ingredients)
+            {
+                ingredient.IngredientAmount *= scaleAmount;
+            }
+
+            // Recalculate total calories
+            calculateTotalCalories();
         }
 
-        //Method to change the scale of the recipe
+        // Method to reset the scale of the recipe
         public void recipeScaleReset()
         {
-            // Reset the scale of a recipe
+            // Reset the current scale amount to the original scale amount
             currentScaleAmount = ogScaleAmount;
+
+            // Adjust ingredient amounts back to the original scale
+            foreach (var ingredient in ingredients)
+            {
+                ingredient.IngredientAmount = ingredient.IngredientAmount / currentScaleAmount;
+            }
+
+            // Recalculate total calories
             calculateTotalCalories();
-
         }
-
     }
 }
-
 /*
  * This is my GitHub repository in case you can't access it through the word doc
-* https://github.com/ST10398576/ST10398576_PROG6112_POE
-*/
+ * https://github.com/ST10398576/ST10398576_PROG6112_POE_Part2
+ */
